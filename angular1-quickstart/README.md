afba2841d738b7f11b956142627730f3b6d5897e
franciov <f.iovine@gmail.com>
2016-01-08 17:22:15 +0000

    implemented the angular 1 quickstart app, based on the angular 2 tutorial for #4

---
+++ angular1-quickstart/README.md
@@ -0,0 +1,15 @@
+# Angular1 quickstart app
+
+App based on [the official Angular 2 quick tutorial](https://angular.io/docs/js/latest/quickstart.html), implementing it using Angular 1
+
+## Configuration
+
+```sh
+npm install
+```
+
+## How to run the app
+
+```sh
+npm start
+```
---
+++ angular1-quickstart/app/app.component.js
@@ -0,0 +1,22 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .directive('myApp', AppComponent);
+
+    AppComponent.$inject = [];
+
+    /* @ngInject */
+    function AppComponent() {
+        // Usage: <my-app>Loading...</my-app>
+        //
+        // Creates: prints a sample text
+        //
+        var directive = {
+            restrict: 'E',
+            template: '<h1>My First Angular 1 App</h1>'
+        };
+        return directive;
+    }
+})();
