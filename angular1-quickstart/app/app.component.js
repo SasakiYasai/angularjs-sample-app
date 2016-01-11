5a4744d41f112754174a0f7ca1e8b5dc79597395
franciov <f.iovine@gmail.com>
2016-01-11 15:03:33 +0000

    formattation

--- angular1-quickstart/app/app.component.js
+++ angular1-quickstart/app/app.component.js
@@ -1,22 +1,22 @@
 (function() {
-    'use strict';
+  'use strict';
 
-    angular
-        .module('app')
-        .directive('myApp', AppComponent);
+  angular
+    .module('app')
+    .directive('myApp', AppComponent);
 
-    AppComponent.$inject = [];
+  AppComponent.$inject = [];
 
-    /* @ngInject */
-    function AppComponent() {
-        // Usage: <my-app>Loading...</my-app>
-        //
-        // Creates: prints a sample text
-        //
-        var directive = {
-            restrict: 'E',
-            template: '<h1>My First Angular 1 App</h1>'
-        };
-        return directive;
-    }
+  /* @ngInject */
+  function AppComponent() {
+    // Usage: <my-app>Loading...</my-app>
+    //
+    // Creates: prints a sample text
+    //
+    var directive = {
+      restrict: 'E',
+      template: '<h1>My First Angular 1 App</h1>'
+    };
+    return directive;
+  }
 })();
